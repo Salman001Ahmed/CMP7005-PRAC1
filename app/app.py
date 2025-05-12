7f0b3fc8af06f8954b9fc3c1217c9f154ba3f965
Salman001Ahmed <st20313619@outlook.cardiffmet.ac.uk>
2025-05-12 09:21:49 +0530

    Initialise the GUI app

---
+++ app/app.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("Air Quality Analysis Dashboard")
+st.markdown("""
+Welcome to the **Air Quality EDA App**.  
+Use the sidebar to navigate between:
+
+- 📊 **Data Overview**  
+- 📈 **Exploratory Data Analysis (EDA)**  
+
+Modeling & Prediction will be available soon!
+""")
